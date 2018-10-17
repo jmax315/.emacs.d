124f969aae2de1dae95ad4a44e836e04d758b0a8
John Maxwell <jmax@jmaxhome.com>
2018-10-17 14:28:00 -0400

    Turned on global line numbers.
    Also added .last-package-update-day to .gitignore.

--- init.el
+++ init.el
@@ -17,6 +17,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(explicit-bash-args (quote ("--noediting" "-i" "-l")))
+ '(global-linum-mode t)
  '(read-buffer-completion-ignore-case t)
  '(read-file-name-completion-ignore-case t)
  '(tab-width 4)
