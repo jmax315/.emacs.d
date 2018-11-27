0c6a527a08a26d4a6e1af85c48a74df119a8d30e
John Maxwell <jmaxwell@alarm.com>
2018-11-27 12:23:57 -0500

    Set auto-package-update frequency to every day.

--- init.el
+++ init.el
@@ -40,6 +40,7 @@
 (use-package auto-package-update
   :ensure t
   :config
+  (setq auto-package-update-interval 1)
   (setq auto-package-update-delete-old-versions t)
   (setq auto-package-update-prompt-before-update t)
   (setq auto-package-update-hide-results nil))
