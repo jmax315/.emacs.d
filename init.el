698d7ce6c82e4a984add9ee8681b846345c27a98
John Maxwell <jmaxwell@alarm.com>
2019-02-15 12:15:17 -0500

    Added yasnippets-snippets

--- init.el
+++ init.el
@@ -151,6 +151,8 @@
   (add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
   (yas-global-mode 1))
 
+(use-package yasnippet-snippets
+  :ensure t)
 
 (auto-package-update-maybe)
 (put 'narrow-to-region 'disabled nil)
