acbbc442ac429a4065d75b5ca16b1030b4407276
John Maxwell <jmax@jmaxhome.com>
2020-04-15 00:18:11 -0400

    Code to do ispell in dired mode.
    Not hooked up to anything yet.

---
+++ lisp/dired-do-ispell.el
@@ -0,0 +1,10 @@
+(defun dired-do-ispell (&optional arg)
+  (interactive "P")
+  (dolist (file (dired-get-marked-files
+                 nil arg
+                 #'(lambda (f)
+                     (not (file-directory-p f)))))
+    (save-window-excursion
+      (with-current-buffer (find-file file)
+        (ispell-buffer)))
+    (message nil)))
